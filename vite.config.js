c0a2e6df37555c0f9ebb50b2bc76281101ea74b1
Emiliano Perez <emi.perez997@gmail.com>
2023-12-21 11:58:10 -0300

    [ADD] Project init

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default ({ mode }) => {
+  // Using env variables in Vite
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
+    base: "/ecommerce-alkemy/",
     plugins: [react()],
   });
 };
